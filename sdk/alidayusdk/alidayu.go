b9f2562e71d143020221bf9469fa545e40b4e150
kuiki <911yinhui911@163.com>
2017-12-19 00:57:42 +0800

    fix vscode suggest for alidayu sdk

--- sdk/alidayusdk/alidayu.go
+++ sdk/alidayusdk/alidayu.go
@@ -7,22 +7,26 @@ import (
 	"strings"
 )
 
+// AlidayuConfig 阿里大鱼sdk配置
 type AlidayuConfig struct {
 	SignName string
-	// TemplateId string
+	// TemplateID string
 	// TemplateParam map[string]string
 }
 
+// AlidayuSmsCommonTemplateParam 阿里大鱼短信模版
 type AlidayuSmsCommonTemplateParam struct {
 	Code    string `json:"code"`
 	Product string `json:"product"`
 }
 
+// AlidayuSdk 阿里大鱼sdk
 type AlidayuSdk struct {
 	client *alidayu.Client
 	config AlidayuConfig
 }
 
+// NewAlidayu 新建阿里大鱼sdk
 func NewAlidayu(appKey, appSecret string, config AlidayuConfig) (dayu *AlidayuSdk, err error) {
 	dayu = &AlidayuSdk{
 		client: &alidayu.Client{AppKey: appKey, AppSecret: appSecret},
@@ -31,11 +35,13 @@ func NewAlidayu(appKey, appSecret string, config AlidayuConfig) (dayu *AlidayuSd
 	return dayu, nil
 }
 
-func (this *AlidayuSdk) SendMsg(mobile string, templateId string, templateParam interface{}) (err error) {
-	return this.SendMultiMsg([]string{mobile}, templateId, templateParam)
+// SendMsg 发送信息
+func (sdk *AlidayuSdk) SendMsg(mobile string, templateID string, templateParam interface{}) (err error) {
+	return sdk.SendMultiMsg([]string{mobile}, templateID, templateParam)
 }
 
-func (this *AlidayuSdk) SendMultiMsg(mobiles []string, templateId string, templateParam interface{}) (err error) {
+// SendMultiMsg 批量发送信息
+func (sdk *AlidayuSdk) SendMultiMsg(mobiles []string, templateID string, templateParam interface{}) (err error) {
 	for _, v := range mobiles {
 		if len(v) != 11 {
 			return errors.New("Mobile " + v + " length not equal 11")
@@ -48,12 +54,12 @@ func (this *AlidayuSdk) SendMultiMsg(mobiles []string, templateId string, templa
 	params := map[string]string{
 		"method":             "alibaba.aliqin.fc.sms.num.send",
 		"sms_type":           "normal",
-		"sms_free_sign_name": this.config.SignName,
-		"sms_template_code":  templateId,
+		"sms_free_sign_name": sdk.config.SignName,
+		"sms_template_code":  templateID,
 		"sms_param":          string(smsParam),
 		"rec_num":            strings.Join(mobiles, ","),
 	}
-	success, result, e := this.client.Exec(params)
+	success, result, e := sdk.client.Exec(params)
 	if e != nil {
 		return errors.New("SendMsg Exec error: " + e.Error())
 	}
