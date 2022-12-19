376051398e392b69e90fe416296bb0735ec5b081
Kuiki <kuiki@kuiki.cn>
2022-12-19 21:07:39 +0800

    add file package for utils

---
+++ utils/file/file.go
@@ -0,0 +1,29 @@
+package file
+
+import "os"
+
+// Exists 判断所给路径文件/文件夹是否存在
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true //os.Stat获取文件信息
+		}
+		return false
+	}
+	return true
+}
+
+// IsDir 判断所给路径是否为文件夹
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return s.IsDir()
+}
+
+// IsFile 判断所给路径是否为文件
+func IsFile(path string) bool {
+	return !IsDir(path)
+}
